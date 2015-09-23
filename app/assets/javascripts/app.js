3c2abfc557ba3d56f4154b63ae46b08ca71f8ad3
David Alvarado <davidealva@gmail.com>
2015-09-23 01:58:26 -0600

    Completed feed view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,10 +2,26 @@
 
 var app = angular.module('app', ['ngResource']);
 
+app.controller('formCtrl', ['$scope', function($scope){
+  $scope.step = 1;
+
+  $scope.nextStep = function() {
+      $scope.step++;
+  }
+
+  $scope.prevStep = function() {
+      $scope.step--;
+  }
+
+  $scope.submitForm = function() {
+      // submit code goes here
+  }
+}]);
+
 app.controller('indexCtrl', function($scope, $log, $http) {
 
   $http.get("./buyers.json").success(function(data) {
   	$scope.buyers = data;
-  });
+ 	});
 
 });
